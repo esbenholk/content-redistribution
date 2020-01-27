580a1f4b17f5bd012abd68a1032aeadeccc2e1a8
Esben Holk <esbenholknielsen@gmail.com>
2020-01-27 15:50:28 +0100

    removed address for server on socket.io

--- index.js
+++ index.js
@@ -11,9 +11,10 @@ const path = require("path");
 const s3 = require("./s3");
 const { s3Url } = require("./config.json");
 const server = require("http").Server(app);
-const io = require("socket.io")(server, {
-    origins: "contentredistribution.herokuapp.com"
-});
+// const io = require("socket.io")(server, {
+//     origins: "contentredistribution.herokuapp.com"
+// });
+const io = require("socket.io")(server);
 
 app.use(compression());
 app.use(express.static("./public"));
